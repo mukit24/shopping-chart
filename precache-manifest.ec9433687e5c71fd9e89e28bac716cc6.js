9e452ecda4593c04fa5054cfaa6cac0c6dd242ac
mukit hasan pranto <mukithasanpranto4@gmail.com>
2020-05-27 19:12:19 +0600

    Updates

---
+++ precache-manifest.ec9433687e5c71fd9e89e28bac716cc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3cc77e67a29831889f85067627c9d07",
+    "url": "/shopping-chart/index.html"
+  },
+  {
+    "revision": "a5a8bd582c1455536a6a",
+    "url": "/shopping-chart/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3d33e32becd7348b71b5",
+    "url": "/shopping-chart/static/js/2.f0fe21bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shopping-chart/static/js/2.f0fe21bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5a8bd582c1455536a6a",
+    "url": "/shopping-chart/static/js/main.b132382e.chunk.js"
+  },
+  {
+    "revision": "ad121eeddbc1f568035c",
+    "url": "/shopping-chart/static/js/runtime-main.348cfa34.js"
+  }
+]);
